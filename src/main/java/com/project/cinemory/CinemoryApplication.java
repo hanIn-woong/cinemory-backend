e3e27c3b537188c203845bf7889334b1b28f9ad4
I U Han <hiu8525@gmail.com>
2026-03-31 14:14:39 +0900

    chore: spring boot 초기 환경 세팅

---
+++ src/main/java/com/project/cinemory/CinemoryApplication.java
@@ -0,0 +1,13 @@
+package com.project.cinemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cinemory/CinemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cinemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
