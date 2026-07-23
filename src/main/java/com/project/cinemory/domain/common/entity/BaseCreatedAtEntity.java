92a9bb90ddc3bb868d453947c917ae434062a812
inung_han <hiu8525@gmail.com>
2026-07-23 15:14:26 +0900

    feat: createdAt 베이스 엔티티 수정

--- src/main/java/com/project/cinemory/domain/common/entity/BaseCreatedAtEntity.java
+++ src/main/java/com/project/cinemory/domain/common/entity/BaseCreatedAtEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -15,9 +14,6 @@ import java.time.LocalDateTime;
 @Getter
 public abstract class BaseCreatedAtEntity {
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
